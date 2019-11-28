5b736836b4b88cf4d8dd15653acac15a83d8de57
vagrant <vagrant@vagrant.vm>
2019-11-28 11:11:37 +0000

    Connect the database in

--- database_setup.py
+++ database_setup.py
@@ -5,4 +5,9 @@ from sqlachemy.ext.declarative import declarative_base
 from sqlachemy.orm import relationship
 from sqlachemy import create_engine
 
+Base = declarative_base()
+
+engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
+Base.metadata.create_all(engine)
+
 
