51693cb527be4fa6d887de7c2373e682176c88a0
vagrant <vagrant@vagrant.vm>
2019-11-28 11:07:31 +0000

    Import the necessary module for configuration database setup

---
+++ database_setup.py
@@ -0,0 +1,8 @@
+import os
+import sys
+from sqlachemy import Column, ForeignKey, Integer, String
+from sqlachemy.ext.declarative import declarative_base
+from sqlachemy.orm import relationship
+from sqlachemy import create_engine
+
+
