267427fb385dafa21520ddda611e464d946df3b5
vagrant <vagrant@vagrant.vm>
2019-11-28 13:24:54 +0000

    Add the html output to the post method

--- web_server.py
+++ web_server.py
@@ -56,12 +56,28 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
 	def do_POST(self):
 		try:
+			
 
 			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
 			if ctype == 'multipart/form-data':
 				fields = cgi.parse_multipart(self.rfile, pdict)
 				message_content = fields.get('message')
 
+			output = ""
+			output += "<html><body>"
+			output += "<h2> Okay, how about this: </h2>"
+			output += "<h1> %s </h1>" % message_content[0]
+
+			output += '''<form method="POST" enctype="multipart/form-data"
+							action="/hello">
+							<h2>What would you like me to say?</h2>
+							<input name="message" type="text">
+							<input type="submit" value="Submit">
+						</form>'''
+			output += '</body></html>'
+
+			self.wfile.write(output)
+			print output
 		except:
 			pass
 
