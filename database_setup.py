9efab4e507c78c2416a7cd6b8140bde10392e157
vagrant <vagrant@vagrant.vm>
2019-11-28 11:26:15 +0000

    Create Restaurant Class that refer to restaurant table in the DB

--- database_setup.py
+++ database_setup.py
@@ -7,6 +7,13 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Restaurant(Base):
+	__tablename__ = 'restaurant'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250), nullable=False)
+
+
 engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
 Base.metadata.create_all(engine)
 
