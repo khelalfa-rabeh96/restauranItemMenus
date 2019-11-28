9c74a6bf28c7dcbee7da35b73010372651c17735
vagrant <vagrant@vagrant.vm>
2019-11-28 13:25:30 +0000

    Send the response, headers

--- web_server.py
+++ web_server.py
@@ -57,6 +57,9 @@ class WebServerHandler(BaseHTTPRequestHandler):
 	def do_POST(self):
 		try:
 			
+			self.send_response(301)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
 
 			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
 			if ctype == 'multipart/form-data':
