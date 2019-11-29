e3dbf711ba9e3c81e19a872df9e6abd17f624927
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2019-11-29 15:41:23 +0100

    Create serialize property to return object data

--- database_setup.py
+++ database_setup.py
@@ -25,6 +25,17 @@ class MenuItem(Base):
 	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 	restaurant = relationship(Restaurant)
 
+	@property
+	def serialize(self):
+		#Return object data in easily seriralieable format
+		return {
+			'name': self.name,
+			'description': self.description,
+			'id' : self.id,
+			'price': self.price,
+			'course': self.course,
+		}
+
 engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
 Base.metadata.create_all(engine)
 
