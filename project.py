945cfaefc34f97314636778998d64ab8bea18211
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2019-11-29 14:18:31 +0100

    Add the Route to ceate a new menu

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -17,11 +17,24 @@ def HelloWorld():
 	return "Hello World"
 
 @app.route('/restaurants/<int:restaurant_id>')
-def restauranMenu(restaurant_id):
+def restaurantMenu(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
 	items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
 
 	return render_template('menu.html', restaurant = restaurant, items = items)
+
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+	if request.method == 'POST':
+		newItem = MenuItem(name = request.form['name'], 
+			restaurant_id = restaurant_id)
+
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+
+	else:
+		return render_template('newMenuItem.html', restaurant_id = restaurant_id)
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
 def editMenu(restaurant_id, menu_id):
