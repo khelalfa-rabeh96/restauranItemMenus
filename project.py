b9eb0a80318ed4db1f89a7dbebe83b6ae640feca
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2019-11-29 12:07:07 +0100

    Write the necessary code to use our DB

--- project.py
+++ project.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
+Base.metadata.bind = engine
+
+DBsession = sessionmaker(bind=engine)
+session = DBsession()
+
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
