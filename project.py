cfb3d736bb348500e07a6dcd98ff43fc8b134b65
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2019-11-29 12:34:46 +0100

    Add a route for a restaurant menu

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -15,6 +15,13 @@ session = DBsession()
 @app.route('/hello')
 def HelloWorld():
 	return "Hello World"
+
+@app.route('/restaurants/<int:restaurant_id>')
+def restauranMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+
+	return render_template('menu.html', restaurant = restaurant, items = items)
 
 if __name__ == '__main__':
 	app.debug = True
