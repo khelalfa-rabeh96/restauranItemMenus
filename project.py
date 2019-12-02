661163fdd4c9bbe234c041e14a278823a0ee5ee1
vagrant <vagrant@vagrant.vm>
2019-12-02 12:33:48 +0000

    verify if the access_token is valid for this app

--- project.py
+++ project.py
@@ -80,7 +80,16 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
     
+
    
 
 
